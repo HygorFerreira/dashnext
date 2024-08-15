a5f0832f2e5786479a93239743595e6d59ab68ab
Hygor Ferreira <hygor09@gmail.com>
2024-08-15 17:18:54 -0300

    1/3

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+export default function Home(){
+  return(
+    <main>
+      <h1 className="text-2xl">Projeto Dashboard</h1>
+      <Button>
+        Teste 
+      </Button>
+    </main>
+  )
+}
